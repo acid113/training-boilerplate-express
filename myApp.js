4f14d99d5514fa0a3b5246c4995eba888355f228
Marvin Estacio <maverick_mle@yahoo.com>
2021-11-10 14:51:50 +0800

    returning arrow functions for checker

--- myApp.js
+++ myApp.js
@@ -7,18 +7,18 @@ dotenv.config();
 
 // middlewares
 app.use("/public", express.static(path.join(__dirname, "/public")));
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
   console.log(`${req.method} ${req.path} - ${req.ip}`);
   next();
 });
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
   const absolutePath = path.join(__dirname, "/views/index.html");
   res.sendFile(absolutePath)
 })
 
 const message = 'Hello json';
-app.get('/json', function (req, res) {
+app.get('/json', (req, res) => {
   res.json({
     "message": process.env.MESSAGE_STYLE === 'uppercase' ? message.toUpperCase() : message 
   })
