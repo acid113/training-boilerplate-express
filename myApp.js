063fdf1a4e47ed6c0eb1e45678014df9346eaadd
Marvin Estacio <maverick_mle@yahoo.com>
2021-11-10 10:58:45 +0800

    updated to serve static assets

--- myApp.js
+++ myApp.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const path = require("path");
 const app = express();
 
+app.use("/public", express.static(path.join(__dirname, "/public")));
+
 app.get('/', (req, res) => {
-  const absolutePath = __dirname + "/views/index.html";
+  const absolutePath = path.join(__dirname, "/views/index.html");
   res.sendFile(absolutePath)
 })
 
