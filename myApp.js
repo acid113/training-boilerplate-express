a891a4c4f8380e0ed900e1a1b1cbce9fb0394809
Marvin Estacio <maverick_mle@yahoo.com>
2021-11-10 14:43:15 +0800

    replace all arrow functions

--- myApp.js
+++ myApp.js
@@ -7,18 +7,18 @@ dotenv.config();
 
 // middlewares
 app.use("/public", express.static(path.join(__dirname, "/public")));
-app.use((req, res, next) => {
+app.use(function (req, res, next) {
   console.log(`${req.method} ${req.path} - ${req.ip}`);
   next();
 });
 
-app.get('/', (req, res) => {
+app.get('/', function (req, res) {
   const absolutePath = path.join(__dirname, "/views/index.html");
   res.sendFile(absolutePath)
 })
 
 const message = 'Hello json';
-app.get('/json', (req, res) => {
+app.get('/json', function (req, res) {
   res.json({
     "message": process.env.MESSAGE_STYLE === 'uppercase' ? message.toUpperCase() : message 
   })
